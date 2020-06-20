0eabd3bdc7f41c7641873d2b7e92bdeac3529378
ScreamingDev <noreply@example.org>
2020-06-20 11:41:09 +0200

    dev: Removed unused functionality
    * jump
    * crouch
    * checking if airborne or grounded

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -8,7 +8,6 @@ namespace UnityStandardAssets.Characters.ThirdPerson
     public class ThirdPersonUserControl : MonoBehaviour
     {
         private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
-        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
         private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
@@ -25,9 +24,6 @@ namespace UnityStandardAssets.Characters.ThirdPerson
                 return;
             }
             
-            // get the transform of the main camera
-            m_Cam = Camera.main.transform;
-
             // get the third person character ( this should never be null due to require component )
             m_Character = GetComponent<ThirdPersonCharacter>();
         }
